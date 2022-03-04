2bfa1e02c8da1fb2636b1d792225c55490c7e49f
Asadbek <Asadbek200525>
2022-03-04 21:24:39 +0300

    githubga joylash

--- handlers/users/post.py
+++ handlers/users/post.py
@@ -1,19 +1,20 @@
-from aiogram import types
-
-from loader import dp,db,bot
-
-from states.holatlar import Forma
-# Echo bot
-@dp.message_handler(commands='post',chat_id=1035757120)
-async def bot_echo(message: types.Message):
-    await message.answer(text='Postni yuborish uchun tashlan')
-    await Forma.post_qabul_qilish.set()
-
-@dp.message_handler(chat_id=1035757120,state=Forma.post_qabul_qilish)
-async def bot_echo(message: types.Message):
-    post = message.text
-
-    userlar = db.select_all_foydalanuvchilar()
-    await message.answer(text=f'{userlar}')
+# from aiogram import types
+#
+# from loader import dp,db,bot
+#
+# from states.holatlar import Forma
+# # Echo bot
+# @dp.message_handler(commands='post',chat_id=1035757120)
+# async def bot_echo(message: types.Message):
+#     await message.answer(text='Postni yuborish uchun tashlan')
+#     await Forma.post_qabul_qilish.set()
+#
+# @dp.message_handler(chat_id=1035757120,state=Forma.post_qabul_qilish)
+# async def bot_echo(message: types.Message):
+#     post = message.text
+#
+#     userlar = db.select_all_foydalanuvchilar()
+#     for user in userlar:
+#         await bot.send_message(chat_id=user[4],text=post)
 
 
