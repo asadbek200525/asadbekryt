2f25834887f6447c839624ce668f627e9d34ac42
Asadbek <Asadbek200525>
2022-03-24 13:58:37 +0300

    githubga joylash

---
+++ keyboards/default/telnomer.py
@@ -0,0 +1,13 @@
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
+
+russpasport = ReplyKeyboardMarkup(
+    keyboard=[
+       [
+           KeyboardButton(text='Qiwi indinfikatsiya 0 dan organish'),
+       ],
+        [
+            KeyboardButton(text='Russ pasport olish')
+        ]
+    ],
+    resize_keyboard=True
+)
