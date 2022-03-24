947a63388d0b7b12c3d7a15e57abed94ddd54e5b
Asadbek <Asadbek200525>
2022-03-24 14:21:04 +0300

    githubga joylash

--- handlers/users/qiwi.py
+++ handlers/users/qiwi.py
@@ -1,4 +1,6 @@
 from aiogram import types
+from aiogram.types import CallbackQuery
+
 from keyboards.default.telnomer import russpasport
 from keyboards.inline.psport import nomer
 
@@ -19,5 +21,9 @@ from loader import dp
 # Echo bot
 @dp.message_handler(text='Russ pasport olish')
 async def bot_echo(message: types.Message):
-    await message.answer_photo(photo='https://t.me/sinov12345987/147',reply_markup=nomer)
+    await message.answer_photo(photo='https://t.me/sinov12345987/147',caption='Id boyicha raqamlarni tanlang' ,reply_markup=nomer)
+
+@dp.callback_query_handler(text='1')
+async def bot_echo(message: CallbackQuery):
+    await message.message.answer_document(document='https://t.me/sinov12345987/148')
 
